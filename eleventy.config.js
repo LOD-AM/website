a587bca22df97f5171ba78875fc795524d039aee
Fabian Fricke <33317518+FFricke@users.noreply.github.com>
2026-07-10 13:18:44 +0200

    Add standard h3 headline with "Title of the source" prefix

--- eleventy.config.js
+++ eleventy.config.js
@@ -221,12 +221,10 @@ export default async function(eleventyConfig) {
       title = customTitle || (await getDOITitle(url)) || displayUrl;
     }
     
-    return '<a href="' + url + '" class="link-preview-card academic-link" style="display: block; text-decoration: none; margin: 1em 0; background-color: #d2b48c; border: 2px solid black; padding: 0.5em 1em 1em 1em; border-radius: 8px;">' +
-      '<div style="display: flex; align-items: center; gap: 1em;">' +
-        '<img src="/img/literature-logo.svg" alt="Literature" style="width: 200px; height: auto;" loading="lazy" />' +
-        '<span style="color: #333; font-weight: bold;">' + title + '</span>' +
-      '</div>' +
-      '<div style="color: #555; font-size: 0.9em; margin-top: 0.5em;">' + displayUrl + '</div>' +
+    return '<a href="' + url + '" class="link-preview-card academic-link" style="display: block; text-decoration: none; color: inherit; margin: 1em 0; background-color: #d2b48c; border: 2px solid black; padding: 0.75em; border-radius: 8px;">' +
+      '<img src="/img/literature-logo.svg" alt="Literature" style="max-width: 50px; height: auto; border-radius: 8px; margin-bottom: 0.5em;" loading="lazy" />' +
+      '<h3 style="color: #3a3a3a; font-size: 1.1em; margin: 0.5em 0;">Title of the source: ' + title + '</h3>' +
+      '<div style="color: #3a3a3a; font-size: 1.1em;">' + displayUrl + '</div>' +
     '</a>';
   });
 };
